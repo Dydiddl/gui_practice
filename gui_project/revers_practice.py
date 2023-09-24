f2219018e5a33c46ced4cb9364d6bcaecb969967
Dydiddl <dydiddl@naver.com>
2023-09-24 17:34:31 +0900

    Finish Practice GUI

--- gui_project/revers_practice.py
+++ gui_project/revers_practice.py
@@ -9,3 +9,15 @@ print("리스트 2 뒤집기 전 : ", lst2)
 lst3 = reversed(lst2)
 print("리스트 2 뒤집은 후 : ", lst2)
 print("리스트 3 : ", list(lst3))
+
+kor = ['사과', '바나나', '오렌지']
+eng = ['apple', 'banana', 'orange']
+
+print(list(zip(kor, eng)))
+
+mixed = [('사과', 'apple'), ('바나나', 'banana'), ('오렌지', 'orange')]
+print(list(zip(*mixed)))
+
+kor2, eng2 = zip(*mixed)
+print(kor2)
+print(eng2)
